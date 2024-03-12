59fce9c5d542fd02857cdfe4ac1820303cc694ee
lucasdpontes <dpontes.lucas@gmail.com>
2024-03-12 20:42:24 -0300

    Correction query

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,7 +1,10 @@
 SELECT
 
-ROUND(SAFE_DIVIDE(SUM(turnover),COUNT(DISTINCT(date_date))),2) AS daily_turnover
+date_date
+,ROUND(SAFE_DIVIDE(SUM(turnover),COUNT(DISTINCT(date_date))),2) AS daily_turnover
 
 FROM course16v1.gwz_sales
+
+GROUP BY date_Date
 
 --Write a query to obtain the daily turnover.
