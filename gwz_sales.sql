a0f8253ac769ff89fb53f57d3fca2f5043d460df
lucasdpontes <dpontes.lucas@gmail.com>
2024-03-12 21:19:32 -0300

    Add purchase_cost

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,10 +1,17 @@
-SELECT
+WITH sales_price AS
+(SELECT
+s.turnover
+,s.date_date
+,s.products_id
+,s.qty
+,p.purchase_price
+FROM course16v1.gwz_sales s
+LEFT JOIN `neural-tangent-411113.course16v1. gwz_product` p
+  ON s.products_id = p.products_id)
 
+SELECT
 date_date
 ,ROUND(SAFE_DIVIDE(SUM(turnover),COUNT(DISTINCT(date_date))),2) AS daily_turnover
-
-FROM course16v1.gwz_sales
-
-GROUP BY date_Date
-
---Write a query to obtain the daily turnover.
+,ROUND(SUM(purchase_price),2) AS purchase_cost
+FROM sales_price
+GROUP BY date_date
